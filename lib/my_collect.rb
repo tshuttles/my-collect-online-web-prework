f18bba150c6540e949b98bb324e409ed46d61d05
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-13 02:03:31 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(collection)
+  x = 0 
+  new_collection = [] 
+  while x < collection.length 
+    new_collection << yield(collection[x]) 
+    x += 1 
+  end 
+  new_collection
+end 
