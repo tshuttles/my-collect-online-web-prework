b5ca456bd94314193dcc296ea135d99657a7e4df
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-13 01:52:38 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   x = 0 
   new_collection = [] 
   while x < collection.length 
-    new_collection << yield()
+    new_collection << yield(collection[x]) 
     x += 1 
     end 
   end 
